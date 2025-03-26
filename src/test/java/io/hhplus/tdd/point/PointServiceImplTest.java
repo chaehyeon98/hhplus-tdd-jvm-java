5e732418c69bc872dad8262579fdfbef2b04bfe2
유채현 <char6127@gmail.com>
2025-03-27 00:53:07 +0900

    포인트조회 use Mockito

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceImplTest.java
@@ -0,0 +1,51 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PointServiceImplTest {
+
+    @Mock
+    private PointService pointService;
+
+    /*
+    * 포인트조회 id 0으로 조회
+    * error 생성불가
+    * */
+    @Test
+    void serchPoint_id0() throws IllegalAccessError {
+
+        //when 동작 검증
+        //가상의객체 PointService에서 동작했을시(searchPoint) 생성되어야되는 결과
+        Mockito.when(pointService.serchPoint(0L)).thenThrow(new IllegalAccessError("생성불가"));
+
+        //then 결과검증
+        IllegalAccessError exception = Assertions.assertThrows(IllegalAccessError.class, () -> pointService.serchPoint(0L));
+
+        Assertions.assertEquals("생성불가", exception.getMessage());
+
+    }
+
+    /*
+     * 포인트조회 id 101으로 조회
+     * error 생성불가
+     * */
+    @Test
+    void serchPoint_id101() throws IllegalAccessError {
+
+        //when 동작 검증
+        //가상의객체 PointService에서 동작했을시(searchPoint) 생성되어야되는 결과
+        Mockito.when(pointService.serchPoint(101L)).thenThrow(new IllegalAccessError("생성불가"));
+
+        //then 결과검증
+        IllegalAccessError exception = Assertions.assertThrows(IllegalAccessError.class, () -> pointService.serchPoint(101L));
+
+        Assertions.assertEquals("생성불가", exception.getMessage());
+    }
+
+}
